9a6ac2dabe414fb348852d90fd9c6fed119ba363
Saurav Kumar <csaurav2014@gmail.com>
2025-04-10 20:59:35 +0530

    Navbar work

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -5,27 +5,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-	
+
 	@GetMapping("/home")
 	public String home() {
-		
+
 		System.out.println("Home page handler");
 		return "home";
 	}
-	
-	//about
+
+	// about
 	@GetMapping("/about")
 	public String aboutPage() {
 		System.out.println("About page loading");
 		return "about";
 	}
-	
+
 	// services
 	@GetMapping("/services")
 	public String servicesPage() {
 		System.out.println("services page loading");
 		return "services";
 	}
-	
+
+	// Contact Page
+	@GetMapping("/contact")
+	public String contactPage() {
+		System.out.println("contact page loading");
+		return new String("contact");
+	}
+
+	// LogIn Page
+	@GetMapping("/login")
+	public String login() {
+		System.out.println("login page loading");
+		return new String("login");
+	}
+
+	// SignUp Page
+	@GetMapping("/signup")
+	public String signup() {
+		System.out.println("signup page loading");
+		return new String("register");
+	}
 
 }
