0a283bc3ff01e4f5ed259c62e25e7bea392bf10a
Saurav Kumar <csaurav2014@gmail.com>
2025-06-15 23:03:04 +0530

    Made Add contact form for number save

---
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -0,0 +1,17 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/contacts")
+public class ContactController {
+	
+	//add contact page:handler
+	@GetMapping("/add")
+	public String addContactView() {
+		return "user/add_contact";
+	}
+
+}
--- src/main/java/com/scm/controllers/UserController.java
+++ src/main/java/com/scm/controllers/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.scm.entities.User;
